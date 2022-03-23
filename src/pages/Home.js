035912ac3665ff066aee730534b3d5290fc9f39d
Shawn Analla <shawnanalla@gmail.com>
2022-03-23 15:49:47 -0700

    home page css wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import '../styles/Home.css'
+import bcsadventures from '../assets/images/bcs-adventures-logo.png'
 
 function Home() {
     return (
         <section>
             <div className='big-name'>
-                <h2>Shawn Analla</h2>
+                <h1>Shawn Analla</h1><h5>Jr Web Developer</h5>
             </div>
 
             <div className='card-container row'>
                 <div className="card">
-                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    {/* <img className="card-img-top" src={bcsadventures} alt="Card image cap" /> */}
                     <div className="card-body">
                         <p className="card-text">About Card</p>
+                        <div className='card-description-layer'>
+                        <p className='card-description'>Learn more about who I am, what I do, and my aspirations.</p>
+                        </div>
                     </div>
                 </div>
 
@@ -20,6 +24,9 @@ function Home() {
                     <img className="card-img-top" src="..." alt="Card image cap" />
                     <div className="card-body">
                         <p className="card-text">Portfolio Card</p>
+                        <div className='card-description-layer'>
+                        <p className='card-description'>View some of my favorite projects. </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -28,13 +35,19 @@ function Home() {
                     <img className="card-img-top" src="..." alt="Card image cap" />
                     <div className="card-body">
                         <p className="card-text">Contact Card</p>
+                        <div className='card-description-layer'>
+                        <p className='card-description'>Find details about how to get in contact with me.</p>
+                        </div>
                     </div>
                 </div>
 
                 <div className="card">
-                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    <img className="card-img-top" src="" alt="Card image cap" />
                     <div className="card-body">
                         <p className="card-text">Personal Card</p>
+                        <div className='card-description-layer'>
+                        <p className='card-description'>Some cool stuff of mine including photography, playlists, and screenshots from games I love.</p>
+                        </div>
                     </div>
                 </div>
             </div>
